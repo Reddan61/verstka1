5e70f48b06428acebaa8d07d5690d2cccf3428c1
Danil Sotskov <Reddan61@yandex.ru>
2020-12-05 19:16:17 +0300

    Updates

---
+++ precache-manifest.5286efd458a8c94d346ee07d473d035f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aeb21d9946295ce36e4fc3baa023906",
+    "url": "/verstka1/index.html"
+  },
+  {
+    "revision": "6851ffa1ed179ba17aac",
+    "url": "/verstka1/static/css/main.b096c8ed.chunk.css"
+  },
+  {
+    "revision": "f505314968efe1edf37d",
+    "url": "/verstka1/static/js/2.f204a647.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/verstka1/static/js/2.f204a647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6851ffa1ed179ba17aac",
+    "url": "/verstka1/static/js/main.af746e32.chunk.js"
+  },
+  {
+    "revision": "20ac3c45c236f09d2428",
+    "url": "/verstka1/static/js/runtime-main.4686e65d.js"
+  },
+  {
+    "revision": "4d47bba4ed96776c92cca68384e54279",
+    "url": "/verstka1/static/media/Group 90.4d47bba4.png"
+  },
+  {
+    "revision": "16de9a245004a8ae50d9edb9ac421e84",
+    "url": "/verstka1/static/media/designedImg.16de9a24.png"
+  },
+  {
+    "revision": "db9d7cb9eed214e21fda0d3775d4599e",
+    "url": "/verstka1/static/media/human2.db9d7cb9.png"
+  },
+  {
+    "revision": "d551a91000befd544899a341cbb92324",
+    "url": "/verstka1/static/media/img.d551a910.png"
+  },
+  {
+    "revision": "54b28d9e2c9f7389887a97ae0f82a2c0",
+    "url": "/verstka1/static/media/man.54b28d9e.png"
+  },
+  {
+    "revision": "5fcca9504dece315812c04539622662e",
+    "url": "/verstka1/static/media/map.5fcca950.png"
+  },
+  {
+    "revision": "37c59fe521fd530dec7348c8eef3eb58",
+    "url": "/verstka1/static/media/particles2.37c59fe5.png"
+  },
+  {
+    "revision": "677644220409b5fa45b444d3f9748749",
+    "url": "/verstka1/static/media/phone.67764422.png"
+  },
+  {
+    "revision": "8942a37c0d448e30e153a51a01a9933f",
+    "url": "/verstka1/static/media/phone.8942a37c.png"
+  }
+]);
